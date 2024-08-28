39b54284232c18edd733f2896faa618dd8085f99
Juan Manuel Guerrero Arteaga <78865058+juanmgart92@users.noreply.github.com>
2024-08-27 23:46:56 -0500

    chore: Update .gitignore and remove unnecessary files

--- JPMGUACHUCAL/buscador.py
+++ JPMGUACHUCAL/buscador.py
@@ -1,28 +1,42 @@
-import pandas as pd
 import os
+import pandas as pd
+from pymongo import MongoClient
+from urllib.parse import quote_plus
+from dotenv import load_dotenv
 from datetime import date
 import pdfplumber
 
-RUTA_CARPETA_J1PF = r"C:\Users\juanmgart\Documents\BUSCADOR\JPMGUACHUCAL"
-RUTA_EXCEL = r"C:\Users\juanmgart\OneDrive - Tecnologico de Antioquia Institucion Universitaria\Estados_Mami\estados_mami.xlsx"
-SHEET_NAME = "JPMGUACHUCAL"
-CARPETA_PDF = r"C:\Users\juanmgart\Documents\BUSCADOR\JPMGUACHUCAL\pdf"
-CARPETA_REVISION = r"C:\Users\juanmgart\Documents\BUSCADOR\JPMGUACHUCAL\revision"
+load_dotenv()
 
-# Leer el archivo Excel
-df = pd.read_excel(RUTA_EXCEL, sheet_name=SHEET_NAME, dtype={'numero': str, 'radicado': str})
+username = quote_plus(os.getenv("MONGO_USERNAME"))
+password = quote_plus(os.getenv("MONGO_PASSWORD"))
 
-# Iterar sobre cada fila del DataFrame
+pdf_folder = os.getenv("JPMGUACHUCAL_PATH_PDF")
+check_folder = os.getenv("JPMGUACHUCAL_CHECK_PATH")
+
+MONGODB_URI = f"mongodb+srv://{username}:{password}@clusterestados.iarfl.mongodb.net/?retryWrites=true&w=majority&appName=ClusterEstados"
+
+client = MongoClient(MONGODB_URI)
+
+db = client["dbestados"]
+
+# COLECCION DB 
+collection = db["JPMGUACHUCAL"]
+
+data = collection.find()
+data_list = list(data)
+df = pd.DataFrame(data_list)
+
 for index, row in df.iterrows():
     numero = row['numero']
-    radicado = row['radicado']  # Obtener el valor de 'radicado'
+    radicado = row['radicado'] 
     found = False
     found_files = []
     # Iterar sobre cada archivo PDF en la carpeta especificada
-    for filename in os.listdir(CARPETA_PDF):
+    for filename in os.listdir(pdf_folder):
         if filename.endswith(".pdf"):
             # Abrir el archivo y leer su contenido
-            with pdfplumber.open(os.path.join(CARPETA_PDF, filename)) as pdf:
+            with pdfplumber.open(os.path.join(pdf_folder, filename)) as pdf:
                 content = ""
                 for page in pdf.pages:
                     content += page.extract_text()
@@ -30,19 +44,12 @@ for index, row in df.iterrows():
                 if numero in content:
                     found = True
                     found_files.append(filename)
-    # Mover los archivos después de cerrar el archivo PDF
-    """ if found:
-        for found_file in found_files:
-            shutil.move(os.path.join(CARPETA_PDF, found_file), os.path.join(CARPETA_RELACION, found_file)) """
+    
     # Escribir la información relevante en un archivo .txt
-    with open(os.path.join(CARPETA_REVISION, f'{date.today()}_revision_JPMGUACHUCAL.txt'), 'a') as txt_file:
+    with open(os.path.join(check_folder, f'{date.today()}_revision.txt'), 'a') as txt_file:
         if found:
-            txt_file.write(f"Se encontró el número {numero} con radicado {radicado} en los archivos: {', '.join(found_files)}\n")
+            txt_file.write(f"Se encontró el numero {numero} con radicado {radicado} en los archivos: {', '.join(found_files)}\n")
         else:
-            txt_file.write(f"No se encontró el número {numero} con radicado {radicado} en ningún archivo.\n")
+            txt_file.write(f"No se encontro el número {numero} con radicado {radicado} en ningun archivo.\n")
         txt_file.write("\n")
 
-# Eliminar los archivos PDF que no contienen el número buscado
-""" for filename in os.listdir(CARPETA_PDF):
-    if filename.endswith(".pdf"):
-        os.remove(os.path.join(CARPETA_PDF, filename)) """
